969614c614d3076dc83cb674042e98bb1b862075
jessica <jessica.gumowski@epfl.ch>
2021-04-27 08:52:55 +0200

    ranger pid2

---
+++ pid.c
@@ -0,0 +1,46 @@
+/*
+ * pid.c
+ *
+ *  Created on: 27 avr. 2021
+ *      Author: jessica
+ */
+#include <pid.h>
+#include <math.h>
+
+#define Kp				1
+#define Ki				0
+#define Kd				0
+
+
+float calcul_pid(float val1, float val2, float threshold, float max)
+{
+	PID_obj pid;
+	float speed;
+	static float integral = 0;
+	static float previous_error = 0;
+
+	float error = val1-val2;
+
+	if(fabs(error) <= threshold)
+	{
+		integral = 0;
+		speed = 0;
+		return speed;
+	}
+	pid.error = error;
+
+	integral += error;
+	if(integral > max){
+		integral = max;
+	}else if(integral < -max){
+		integral = -max;
+	}
+	pid.integral = integral;
+
+	pid.derivate = error-previous_error;
+
+	speed = Kp*pid.error + Ki*pid.integral + Kd*pid.derivate;
+
+	return speed;
+}
+
